7f9f52b552747115a828beaff20b4bcbfd57c64b
Tim Julien <tjulien@limewire.com>
2010-11-16 17:30:57 -0500

    more installation instructions

--- tool.py
+++ tool.py
@@ -4,7 +4,7 @@ r"""Command-line tool to validate and pretty-print HTML
 
 Usage::
 
-    $ echo '<html><body>Hello World</body></html>' | python -mhtml.tool
+    $ echo '<html><body>Hello World</body></html>' | python -m html.tool
     <html>
      <body>
       Hello World
